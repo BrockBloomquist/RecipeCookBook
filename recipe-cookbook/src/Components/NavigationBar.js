9a2080635cd85355dc31648db77f5317cb206537
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-05-12 02:56:43 -0500

    Styled the NavBar with a SignUp and Login links

--- recipe-cookbook/src/Components/NavigationBar.js
+++ recipe-cookbook/src/Components/NavigationBar.js
@@ -1,34 +1,75 @@
-import Container from "react-bootstrap/Container";
+import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "./CSS Components/NavigationBar.css";
 
-function NavigationBar() {
+const NavigationBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+    <>
+      <Navbar // creating a Navbar component
+        collapseOnSelect // Navbar will collapse on smaller screens
+        expand="lg" // Navbar will expand on larger screens
+        bg="dark" // Navbar will have a light background
+        variant="dark" // Navbar will have a light variant
+        sticky="top" // Navbar will stick to the top of the page
+      >
+        <Navbar.Brand href="/">
+          {" "}
+          {/* Navbar Brand  */}
+          <div className="logo">
+            {" "}
+            {/* logo class to style in css */}
+            <span className="title">Recipe Cookbook</span>{" "}
+            {/* cofo class to style in css */}
+            {/* concept class to style in css */}
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />{" "}
+        {/* Navbar Toggle for reponsive navigation */}
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {" "}
+          {/* Navbar Collapse for reponsive navigation for mobile phones*/}
+          <Nav className="nav-bar-links">
+            {" "}
+            {/* Nav component for navigation */}
+          </Nav>
+          <Nav.Link
+            className="SIGNUPBAR"
+            href="/signup"
+            style={{
+              // inline styling for the Sign Up button
+              backgroundColor: "#fc4903", // orange
+              color: "white",
+              borderRadius: "20px", // rounded corners
+              padding: "10px 20px",
+            }}
+          >
+            SIGN UP!
+          </Nav.Link>
+          <Nav>
+            <Nav.Link
+              eventKey={2}
+              href="/login"
+              style={{
+                // inline styling for the Sign Up button
+                backgroundColor: "#fc4903", // orange
+                color: "white",
+                borderRadius: "20px", // rounded corners
+                padding: "10px 20px 10px",
+                marginLeft: "10px",
+              }}
+            >
+              {" "}
+              {/* eventKey is used to identify the component */}
+              {" Login"}
+              {/* FontAwesomeIcon component for icons(User icon) */}
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      </Navbar>
+    </>
   );
-}
-
+};
 export default NavigationBar;
