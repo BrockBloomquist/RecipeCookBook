485afee895109ed7c6c1bd62473b8f8ef5b84f4f
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-05-17 21:35:59 -0500

    Made some edits to the navigation bar and made the image have an overlay on the home screen over the image.

--- recipe-cookbook/src/Components/Banner.js
+++ recipe-cookbook/src/Components/Banner.js
@@ -1,14 +1,45 @@
+import { Container, Row, Col } from "react-bootstrap";
 import bannerBg from "../Assets/img/banner-bg.jpg";
 import "./CSS Components/Banner.css";
-import React, { useState, useRef } from "react";
-import Button from "react-bootstrap/Button";
-import Overlay from "react-bootstrap/Overlay";
-import Tooltip from "react-bootstrap/Tooltip";
-
+import React, { useState, useRef, useEffect } from "react";
 const Banner = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <div className="banner">
-      <img alt="food" src={bannerBg} />
+      <img
+        className={scrolled ? "scrolled-banner" : ""}
+        alt="bg-banner"
+        src={bannerBg}
+      ></img>
+      <div className="overlay">
+        <Container
+          className="overlay-container"
+          style={{
+            position: "absolute",
+            top: "50%",
+            transform: "translate(0px, -50%)",
+            height: "300px",
+            borderRadius: "35px",
+          }}
+        >
+          <div className="overlay-text">Test</div>
+        </Container>
+      </div>
     </div>
   );
 };
