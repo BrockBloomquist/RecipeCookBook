2518ef2f2e338f67bb88925552e6dfc611249908
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-05-19 17:32:38 -0500

    Implemented the login page and component

--- recipe-cookbook/src/App.js
+++ recipe-cookbook/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Coverpage from "./Pages/Coverpage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Coverpage />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" Component={Coverpage} />
+          <Route path="/login" Component={Login} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
