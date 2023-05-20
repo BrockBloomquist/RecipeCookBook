285d30fe542bda92a4084c9d70acda677b3fcd1b
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-05-19 19:22:54 -0500

    Did some styling to the Login page and setting up the handle submit. Need to setup Firebase

--- recipe-cookbook/src/Pages/Login.js
+++ recipe-cookbook/src/Pages/Login.js
@@ -1,27 +1,39 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
-
+import { Form, Button, Container, Card } from "react-bootstrap";
+import { useState } from "react";
+import "./CSS Pages/Login.css";
 export default function Login() {
+  const [checkbox, setCheckbox] = useState(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(checkbox);
+  }
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Card className="wholecard">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            type="checkbox"
+            label="Remember your username"
+            onChange={(e) => setCheckbox(e.target.checked)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Card>
   );
 }
